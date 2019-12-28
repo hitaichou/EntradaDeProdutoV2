8cf28fe58eb59d0413e7e7c35678d812a8a41e53
hitaichou <holyfernandes@yahoo.co.uk>
2019-12-28 08:31:58 -0300

    Exemplo de Entrada de Produtos versão 2

---
+++ EntradaDeProdutoV2/Produto.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace Exercicio_EntradaSaida_Materia_do_Estoque
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        //void -> não retorna valor de saída, e sim faz uma operação
+        //ela necessita de uma variável de entrada
+        public void AdicionarProdutos(int quantidade)
+        {
+            //Quantidade com "Q" maiúsculo é da entrada do usuário que é usado na classe
+            //quantidade com "q" minúsculo é a alteração da quantidade
+            Quantidade = Quantidade + quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+
+        //Sobreposição        
+        public override string ToString()
+        {
+            return Nome
+                 + ", $ "
+                 + Preco.ToString("F2", CultureInfo.InvariantCulture) //tratamento
+                 + ", "
+                 + Quantidade
+                 + " unidades, Total: $ "
+                 + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture); //chamada da função Valor Total Em Estoque
+        }
+    }
+}
--- EntradaDeProdutoV2/Program.cs
+++ EntradaDeProdutoV2/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Globalization;
 
-namespace EntradaDeProdutoV2
+namespace Exercicio_EntradaSaida_Materia_do_Estoque
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Produto p = new Produto();
+
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            //O "p" já vem formatado com o return da classe Produto
+            //por este motivo não é necessário formatar na chamada abaixo.
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            //Depois de criado a operação na Classe Produto
+            //chamamos a função Adicionar Produtos e passamos a quantidad
+            //que o usuário inputou no programa
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + p);
         }
     }
 }
